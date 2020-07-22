ed03fdc37dcf025bb6072a5ccd7560e3d58e89cc
megamp15 <47288803+megamp15@users.noreply.github.com>
2020-07-21 22:01:22 -0500

    Created percent error for each of the 6 flux to pivot wavelength points from the interpolated flux to pivot points

--- python/SED.py
+++ python/SED.py
@@ -8,10 +8,10 @@ import scipy.interpolate
 from utilities import filterlist_to_filterfiles, specarray_to_counts, isfloat, sp
 
 
-def SED(spectra, filter_list, flux_convert):
+def SED(spectrum, filter_list, flux_convert):
     '''
     Parameters: 
-        spectra      -- The spectrum file you wish to overlay the SED on.
+        spectrum      -- The spectrum file you wish to overlay the SED on.
         filter_list  -- The filters you wish to create the SED points for.
         flux_convert -- flux converion numbers for each filter_list in the same order.
 
@@ -30,70 +30,89 @@ def SED(spectra, filter_list, flux_convert):
 
     # Creates a list of filter files, list of zeropoint values, and list of pivot wavelengths corresponding to the filter list inputted from the parameters.
     filter_file_list, zeropointlist, pivotlist = filterlist_to_filterfiles(
-        filter_list, spectra)
+        filter_list, spectrum)
 
     # Gets spectrum data and separates it into two lists for spectra wavelength and spectra flux.
-    spectra_data = list(map(sp, open('../spectra/'+spectra).readlines()))
-    spectra_wave = []
-    spectra_flux = []
-    for i in range(len(spectra_data)):
-        if isfloat(spectra_data[i][0]) or isfloat(spectra_data[i][1]):
-            spectra_wave.append(float(spectra_data[i][0]))
-            spectra_flux.append(float(spectra_data[i][1]))
+    spectrum_data = list(map(sp, open('../spectra/'+spectrum).readlines()))
+    spectrum_wave = []
+    spectrum_flux = []
+    for i in range(len(spectrum_data)):
+        if isfloat(spectrum_data[i][0]) or isfloat(spectrum_data[i][1]):
+            spectrum_wave.append(float(spectrum_data[i][0]))
+            spectrum_flux.append(float(spectrum_data[i][1]))
 
     # Convert the lists to numpy arrays.
-    spectra_wavelength = np.array(spectra_wave)
-    spectra_flux_dens = np.array(spectra_flux)
-    spectrum = np.column_stack((spectra_wavelength[:], spectra_flux_dens[:]))
+    spectrum_wavelength = np.array(spectrum_wave)
+    spectrum_flux_dens = np.array(spectrum_flux)
+    spectrum_orig = np.column_stack(
+        (spectrum_wavelength[:], spectrum_flux_dens[:]))
 
     # Pass the spectrum information along with the filter file list to specarray_to_counts to get counts (count rates) for each filter.
-    count = specarray_to_counts(spectrum, filter_file_list)
+    count = specarray_to_counts(spectrum_orig, filter_file_list)
 
     # Convert count to flux using flux conversion values
     flux = []
     for idx, i in enumerate(flux_convert):
         flux.append((count[idx]*i)/(pow(10, 16)))
 
-    # Interpolate between the pivot wavelength and flux density 
-    f=scipy.interpolate.interp1d(pivotlist, flux, kind='linear')
-    
-    # Limit the spectrum wavelengths to within the pivotlist range for interpolation
-    spectra_wave_interp = []
-    for i in spectra_wave:
-        if (i > pivotlist[0] and i < pivotlist[-1]):
-            spectra_wave_interp.append(i)
-    spectra_wave_interp=np.array(spectra_wave_interp)
-    flux_interp = f(spectra_wave_interp)
-    spectrum_interp = np.column_stack((spectra_wave_interp, flux_interp))
+    # Interpolate between the pivot wavelength and flux density
+    f = scipy.interpolate.interp1d(
+        pivotlist, flux, kind='linear', fill_value="extrapolate")
+
+    # Wavelength range 1600-8000 Angstroms
+    extrap = [i*10 for i in range(160, 801)]
+
+    # Using the fill_value parameter of scipy interpolate we can extrapolate more values outside the pivot wavelength range
+    flux_interp = f(extrap)
+    spectrum_interp = np.column_stack((extrap, flux_interp))
 
     # Pass the new interpolated spectrum to specarray_to_counts
     count_interp = specarray_to_counts(spectrum_interp, filter_file_list)
 
+    # Convert count to flux using flux conversion values
+    flux_interp = []
+    for idx, i in enumerate(flux_convert):
+        flux_interp.append((count_interp[idx]*i)/(pow(10, 16)))
+
+    print("FILTER LIST: ", filter_list)
+    print("COUNT: ", count)
+    print("INTERP COUNT: ", count_interp)
+    print("FLUX: ", flux)
+    print("INTERP FLUX: ", flux_interp)
+
+    error = []
+    for idx, i in enumerate(count):
+        error.append((np.abs(i-count_interp[idx])/i)*100)
+    print("PERCENT ERROR: ", error)
+
     # Ouput the pivot wavelengths values and corresponding Flux density to csv file.
-    with open('../output/csv/'+spectra[: spectra.find(".")]+'_info.csv', mode='w') as csv_file:
+    with open('../output/csv/'+spectrum[: spectrum.find(".")]+'_info.csv', mode='w') as csv_file:
         writer = csv.writer(csv_file, delimiter=",",
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(["Pivot Wavelength", "Flux Density"])
         for idx, i in enumerate(pivotlist):
             writer.writerow([i, flux[idx]])
 
-    # Plotting pivot wavelength vs flux density
-    plt.plot(spectra_wave, spectra_flux)  # Spectrum data
+    # Plotting pivot wavelength vs flux density along with the interpolated flux densities
+    plt.plot(spectrum_wave, spectrum_flux)  # Spectrum data
     plt.plot(pivotlist, flux, 'r--')  # Red dashes to see the curve
     plt.plot(pivotlist, flux, 'ro')  # Red points to see the particular points
-    plt.xlim(0, pivotlist[-1]+17500)
-    plt.xlabel("Wavelength")
+    plt.plot(pivotlist, flux_interp, 'g--')
+    plt.plot(pivotlist, flux_interp, 'go')
+    plt.xlim(0, pivotlist[-1]+12500)
+    plt.xlabel("Wavelength (Angstroms)")
     plt.ylabel("Flux Density")
-    plt.title("SED for Vega spectrum")
-    # plt.savefig('../output/plots/'+spectra+'_SED.png')
-    # plt.show()
+    plt.title(
+        "SED for " + spectrum[: spectrum.find(".")].capitalize()+" Spectrum")
+    plt.savefig('../output/plots/'+spectrum[: spectrum.find(".")]+'_SED.png')
+    plt.show()
 
 
 def main():
     filter_file_list = ['UVW2', 'UVM2', 'UVW1',  'U', 'B', 'V']
     flux_conversion = [6.03, 8.30, 4.02, 1.44, 1.16, 2.62]
-    spectra = 'vega.dat'
-    SED(spectra, filter_file_list, flux_conversion)
+    spectrum = 'vega.dat'
+    SED(spectrum, filter_file_list, flux_conversion)
 
 
 if __name__ == "__main__":
